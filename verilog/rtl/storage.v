df43e1ddae3b0d36655878144b58b01e82464dfe
manarabdelaty <manarabdelatty@aucegypt.edu>
2021-11-12 17:56:58 +0200

    RO clk is mgmt_clk

--- verilog/rtl/storage.v
+++ verilog/rtl/storage.v
@@ -42,7 +42,6 @@ module storage (
 );
 
     wire [31:0] ro_rdata;
-    wire ro_clk = ~sram_ro_csb ? sram_ro_clk : mgmt_clk; 
     wire ro_csb = ~sram_ro_csb ? sram_ro_csb : mgmt_ena_ro;
     wire [8:0] ro_addr = ~sram_ro_csb ? {1'b1, sram_ro_addr} : {1'b0, mgmt_addr_ro[7:0]};
 
@@ -59,7 +58,7 @@ module storage (
         .din0(mgmt_wdata),
         .dout0(mgmt_rdata[31:0]),
         // MGMT RO port
-        .clk1(ro_clk),
+        .clk1(mgmt_clk),
         .csb1(ro_csb), 
         .addr1(ro_addr),
         .dout1(ro_rdata)
